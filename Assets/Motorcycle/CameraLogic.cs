aa3af5dd3cd93132cbf23cae1a31b076472f82cd
Rulet <knopka-02@mail.ru>
2026-06-21 21:12:39 +0300

    реализация простого передвижения движения объекта и написание скрипта для фиксации камеры к объекту

---
+++ Assets/Motorcycle/CameraLogic.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraLogic : MonoBehaviour
+{
+    public GameObject skmbike;
+    public Camera Camera; 
+    void Awake()
+    {
+        SetCamera();
+    }
+    private void LateUpdate()
+    {
+        SetCamera();
+    }
+    void Update()
+    {
+       
+    }
+    public Vector3 GetPosition(GameObject obj)
+    {
+        return obj.transform.position;
+    }
+    public Quaternion GetRotation(GameObject obj)
+    {
+        return obj.transform.rotation;
+    }
+    public void SetCamera()
+    {
+        Camera.transform.position = GetPosition(skmbike) + new Vector3(0, 1.4f, 0);
+        Camera.transform.rotation = GetRotation(skmbike) * Quaternion.Euler(5f, 1f, 1f);
+    }
+}
--- Assets/Motorcycle/MovementController.cs
+++ Assets/Motorcycle/MovementController.cs
@@ -1,16 +1,44 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject skmbike;
+    private float movespeed = 10f;
+    private float rotationspeed = 100f;
+    private void StartPosition()
+    {
+        skmbike.transform.position = new Vector3(0, 0, 0);
+        skmbike.transform.rotation = Quaternion.Euler(0, 0, 0);
+        skmbike.transform.localScale = new Vector3(1, 1, 1);
+    }
     void Start()
     {
-        
+        StartPosition();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+  
+        float moveDirection = InputWS().y * movespeed * Time.deltaTime;
+        skmbike.transform.Translate(0, 0, moveDirection);
+        float rotationDirection = InputAD().x * rotationspeed * Time.deltaTime;
+        skmbike.transform.Rotate(0, rotationDirection, 0);
+    }
+    private Vector2 InputWS()
+    {
+         if (Keyboard.current.wKey.isPressed)
+            return Vector2.up;
+         if (Keyboard.current.sKey.isPressed)
+            return Vector2.down;
+        else return Vector2.zero;
+    }
+    private Vector2 InputAD()
+    {
+        if (Keyboard.current.dKey.isPressed)
+            return Vector2.right;
+        if (Keyboard.current.aKey.isPressed)
+           return Vector2.left;
+        else return Vector2.zero;
     }
 }
+  
