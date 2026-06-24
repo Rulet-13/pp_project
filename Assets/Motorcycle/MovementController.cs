a75c60403da79674563912c2914aba13e7636ac5
Rulet <knopka-02@mail.ru>
2026-06-24 21:13:55 +0300

    Реализация передвижения через RigidBody, введения методов для обработки ввода, рестарта позиции объекта, что бы тестирование было удобнее. Введение метода для контроля параметра Tensor

--- Assets/Motorcycle/MovementController.cs
+++ Assets/Motorcycle/MovementController.cs
@@ -1,44 +1,109 @@
 ﻿using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public GameObject skmbike;
-    private float movespeed = 10f;
+    private float movespeed = 120f;
     private float rotationspeed = 100f;
+    private Rigidbody rbbike;
+    private bool tens = false;
+    private float moveDirection
+    {
+        get
+        {
+            return InputWS().y * movespeed;
+        }
+
+    }
+    private float rotationDirection
+    {
+        get
+        {
+            return InputAD().x * rotationspeed;
+        }
+    }
     private void StartPosition()
     {
-        skmbike.transform.position = new Vector3(0, 0, 0);
+        skmbike.transform.position = new Vector3(0, 1, 0);
         skmbike.transform.rotation = Quaternion.Euler(0, 0, 0);
-        skmbike.transform.localScale = new Vector3(1, 1, 1);
+        tens = false;
+        TensorControl(tens);
     }
-    void Start()
+    private void Awake()
+    {
+        rbbike = skmbike.GetComponent<Rigidbody>();
+    }
+    private void Start()
     {
         StartPosition();
+
     }
-    void Update()
+    private void FixedUpdate()
     {
-  
-        float moveDirection = InputWS().y * movespeed * Time.deltaTime;
-        skmbike.transform.Translate(0, 0, moveDirection);
-        float rotationDirection = InputAD().x * rotationspeed * Time.deltaTime;
-        skmbike.transform.Rotate(0, rotationDirection, 0);
+        rbbike.linearVelocity = rbbike.transform.TransformDirection(new Vector3(0f, rbbike.linearVelocity.y, moveDirection * Time.fixedDeltaTime));
+        rbbike.angularVelocity = new Vector3(0f, rotationDirection * Time.fixedDeltaTime, 0f);
+        TensorControl(tens);
     }
+
+    private void Update()
+    {
+        Restart();
+    }
+
     private Vector2 InputWS()
     {
-         if (Keyboard.current.wKey.isPressed)
+        if (Keyboard.current.wKey.isPressed)
+        {
+            tens = true;
             return Vector2.up;
-         if (Keyboard.current.sKey.isPressed)
+        }
+        if (Keyboard.current.sKey.isPressed)
+        {
+            tens = true;
             return Vector2.down;
+        }
         else return Vector2.zero;
     }
     private Vector2 InputAD()
     {
-        if (Keyboard.current.dKey.isPressed)
-            return Vector2.right;
-        if (Keyboard.current.aKey.isPressed)
-           return Vector2.left;
+        if (Keyboard.current.dKey.isPressed) return Vector2.right;
+        if (Keyboard.current.aKey.isPressed) return Vector2.left;
         else return Vector2.zero;
     }
+    private void Restart()
+    {
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            StartPosition();
+        }
+    }
+    private void TensorControl(bool t)
+    {
+        t = tens;
+        if (t)
+        {
+            rbbike.automaticInertiaTensor = false;
+            rbbike.inertiaTensor = new Vector3(10, 6, 1);
+
+        }
+        else
+        {
+            rbbike.automaticInertiaTensor = true;
+        }
+    }
+    private void Acceleration(int m)
+    {
+        switch (m)
+        {
+            case (1):
+                break;
+            case (2):
+                break;
+            case (3):
+                break;
+        }
+    }
 }
-  
+
