b5747f8de570ca9213e42b042b7ce5a7e9289d4f
Rulet <knopka-02@mail.ru>
2026-06-28 23:02:40 +0300

    Изменение tens теперь внутри метода InputWS() Реализация наклон объекта вправо/влева при его повороте на AS. Введены переменные для контроля угла наклона, ограничение угла, возврат в изначально положение при отжатии клавиш.

--- Assets/Motorcycle/MovementController.cs
+++ Assets/Motorcycle/MovementController.cs
@@ -1,39 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics.SymbolStore;
+using Unity.VisualScripting;
+using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
     public GameObject skmbike;
-    private float movespeed = 120f;
-    private float rotationspeed = 100f;
+    private float movespeedMaxW = 1700f; private float movespeednow = 0f; private float movespeedMaxS = 500f;
+    private float rotationspeed = 80f; private float slantmax = 20f; private float slantnow = 0f; private float leanSpeed = 20f;
     private Rigidbody rbbike;
     private bool tens = false;
-    private float moveDirection
-    {
-        get
-        {
-            return InputWS().y * movespeed;
-        }
-
-    }
-    private float rotationDirection
-    {
-        get
-        {
-            return InputAD().x * rotationspeed;
-        }
-    }
+    private float moveDirection { get { return InputWS().y * movespeednow; } }
+    private float rotationDirection { get { return InputAD().x * rotationspeed; } }
+    
     private void StartPosition()
     {
         skmbike.transform.position = new Vector3(0, 1, 0);
         skmbike.transform.rotation = Quaternion.Euler(0, 0, 0);
         tens = false;
-        TensorControl(tens);
+        TensorControl();
     }
     private void Awake()
     {
         rbbike = skmbike.GetComponent<Rigidbody>();
+        rbbike.maxAngularVelocity = Vector2.one.magnitude * slantmax;
     }
     private void Start()
     {
@@ -42,9 +33,10 @@ public class NewMonoBehaviourScript : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        rbbike.linearVelocity = rbbike.transform.TransformDirection(new Vector3(0f, rbbike.linearVelocity.y, moveDirection * Time.fixedDeltaTime));
-        rbbike.angularVelocity = new Vector3(0f, rotationDirection * Time.fixedDeltaTime, 0f);
-        TensorControl(tens);
+        rbbike.linearVelocity = (skmbike.transform.forward * moveDirection * Time.fixedDeltaTime) + (Vector3.up * rbbike.linearVelocity.y);
+        skmbike.transform.localRotation = Quaternion.Euler(0, skmbike.transform.localRotation.eulerAngles.y, -slantnow);
+        skmbike.transform.Rotate(0f, rotationDirection * Time.fixedDeltaTime, 0f, Space.World);
+        TensorControl();
     }
 
     private void Update()
@@ -56,21 +48,50 @@ public class NewMonoBehaviourScript : MonoBehaviour
     {
         if (Keyboard.current.wKey.isPressed)
         {
+            movespeednow += 2f;
+            if (movespeednow > movespeedMaxW) movespeednow = movespeedMaxW;
             tens = true;
             return Vector2.up;
         }
-        if (Keyboard.current.sKey.isPressed)
-        {
+        else if (Keyboard.current.sKey.isPressed)
+        {  
+            movespeednow++;
+            if (movespeednow > movespeedMaxS) movespeednow = movespeedMaxS;
             tens = true;
             return Vector2.down;
         }
-        else return Vector2.zero;
+        else
+        {
+            movespeednow--;
+            if (movespeednow < 0) movespeednow = 0;
+            tens = false;
+            return Vector2.zero;
+        }
     }
     private Vector2 InputAD()
     {
-        if (Keyboard.current.dKey.isPressed) return Vector2.right;
-        if (Keyboard.current.aKey.isPressed) return Vector2.left;
-        else return Vector2.zero;
+        if (Keyboard.current.dKey.isPressed && (Keyboard.current.wKey.isPressed || Keyboard.current.sKey.isPressed))
+        {
+            slantnow += leanSpeed * Time.deltaTime;
+            slantnow = Mathf.Clamp(slantnow, -slantmax, slantmax);
+
+            return Vector2.right;
+        }
+
+        if (Keyboard.current.aKey.isPressed && (Keyboard.current.wKey.isPressed || Keyboard.current.sKey.isPressed))
+        {
+            slantnow -= leanSpeed * Time.deltaTime;
+            slantnow = Mathf.Clamp(slantnow, -slantmax, slantmax);
+
+            return Vector2.left;
+        }
+
+        slantnow = Mathf.MoveTowards(
+            slantnow,
+            0,
+            leanSpeed * Time.deltaTime);
+
+        return Vector2.zero;
     }
     private void Restart()
     {
@@ -79,31 +100,18 @@ public class NewMonoBehaviourScript : MonoBehaviour
             StartPosition();
         }
     }
-    private void TensorControl(bool t)
+    private void TensorControl()
     {
-        t = tens;
-        if (t)
+        if (tens)
         {
             rbbike.automaticInertiaTensor = false;
-            rbbike.inertiaTensor = new Vector3(10, 6, 1);
-
+            rbbike.inertiaTensor = new Vector3(10, 10, 1);
         }
         else
         {
             rbbike.automaticInertiaTensor = true;
         }
     }
-    private void Acceleration(int m)
-    {
-        switch (m)
-        {
-            case (1):
-                break;
-            case (2):
-                break;
-            case (3):
-                break;
-        }
-    }
+
 }
 
